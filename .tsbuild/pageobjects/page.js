7573cedcdad7d6daf7caa939af632986f78bf07e
Wiltrouble <wiltrouble@gmail.com>
2021-03-03 00:52:46 -0400

    removing build

--- .tsbuild/pageobjects/page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Page {
-    open(path) {
-        return browser.url(`https://the-internet.herokuapp.com/${path}`);
-    }
-}
-exports.default = Page;
