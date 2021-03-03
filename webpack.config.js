9cbb67a92e9da0c85a7e33ffb20515d6286d8b3d
Wiltrouble <wiltrouble@gmail.com>
2021-03-03 00:00:06 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
- const path = require('path')
+const path = require('path');
 
- module.exports = {
-     entry: './features/*.ts*',
-     output: {
-         path: path.resolve(__dirname, 'dist'),
-         filename: 'main.js',
-     },
-     resolve: {
-        extensions: ['.ts']
-     }
- }
+module.exports = {
+  entry: './features/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
